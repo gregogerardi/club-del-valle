07fa24a23b84d73eca5374f113232b441174f177
Gregorio Gerardi <gregorio@Gregorios-MacBook-Pro-2.local>
2020-12-20 11:56:08 -0300

    first content working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
+  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
