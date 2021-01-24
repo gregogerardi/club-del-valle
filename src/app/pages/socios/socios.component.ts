77bf4f3036494109c1f8c61e8df37e18b2a9adce
gregogerardi <gregorionicolasgerardi@gmail.com>
2021-01-24 18:01:01 -0300

    contacto socios

--- src/app/pages/socios/socios.component.ts
+++ src/app/pages/socios/socios.component.ts
@@ -4,4 +4,8 @@ import { Component } from '@angular/core';
   templateUrl: './socios.component.html',
   styleUrls: ['./socios.component.scss']
 })
-export class SociosComponent {}
+export class SociosComponent {
+  public sendContactEmail(){
+    window.open('mailto:delvallenecochea@hotmail.com?subject=Quiero%20ser%20socio&body=Apellido%20y%20Nombre%3A%0D%0AFecha%20de%20Nacimiento%20(dd%2Fmm%2Faaaa)%3A%0D%0ATipo%20de%20documento%3A%0D%0AN%C3%BAmero%20de%20documento%3A%0D%0ANumero%20de%20telefono%2Fcelular%3A%0D%0ADomicilio%20(calle%20-%20numero)%3A%0D%0ACiudad%3A', '_blank');
+  }
+}
